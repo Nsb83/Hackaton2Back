2e96abe641371bd42bb23c37679bfb70e28872a0
Nicolas Barbier <Nicolas-Barbier_student2018@wilder.school>
2018-12-20 14:23:31 +0100

    Creation projet

---
+++ src/main/java/fr/wild/Hackaton/HackatonApplication.java
@@ -0,0 +1,14 @@
+package fr.wild.Hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonApplication.class, args);
+	}
+
+}
+
